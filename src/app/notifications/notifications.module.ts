5aa718b40b12d0d6a1a4f6a26370f58aa4f4e406
Mdamman <m.damman@students.ephec.be>
2021-12-23 09:07:19 +0100

    notification points utilisateur
    Signed-off-by: Mdamman <m.damman@students.ephec.be>

--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -1,33 +1,34 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ComponentsModule } from '../components/components.module';
+import { ComponentsModule } from "../components/components.module";
 
-import { NotificationsPage } from './notifications.page';
-import { NotificationsResolver } from '../notifications/notifications.resolver';
-import { NotificationsService } from '../notifications/notifications.service';
+import { NotificationsPage } from "./notifications.page";
+import { NotificationsResolver } from "../notifications/notifications.resolver";
+import { NotificationsService } from "../notifications/notifications.service";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../../environments/environment";
+import { DonateService } from "../donate/donate.service";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     ComponentsModule,
+    AngularFireModule.initializeApp(environment.firebase),
     RouterModule.forChild([
       {
-        path: '',
+        path: "",
         component: NotificationsPage,
         resolve: {
-          data: NotificationsResolver
-        }
-      }
-    ])
+          data: NotificationsResolver,
+        },
+      },
+    ]),
   ],
-  declarations: [ NotificationsPage ],
-  providers: [
-    NotificationsResolver,
-    NotificationsService
-  ]
+  declarations: [NotificationsPage],
+  providers: [NotificationsResolver, NotificationsService, DonateService],
 })
 export class NotificationsPageModule {}
--- src/app/notifications/notifications.page.ts
+++ src/app/notifications/notifications.page.ts
@@ -1,41 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription, Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { DonateService } from "../donate/donate.service";
 
 @Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.page.html',
+  selector: "app-notifications",
+  templateUrl: "./notifications.page.html",
   styleUrls: [
-    './styles/notifications.page.scss',
-    './styles/notifications.shell.scss'
-  ]
+    "./styles/notifications.page.scss",
+    "./styles/notifications.shell.scss",
+  ],
 })
 export class NotificationsPage implements OnInit {
-  // Gather all component subscription in one place. Can be one Subscription or multiple (chained using the Subscription.add() method)
-  subscriptions: Subscription;
-
-  notifications: any;
-
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.subscriptions = this.route.data
-    .pipe(
-      // Extract data for this page
-      switchMap((resolvedRouteData: {source: Observable<any>}) => {
-        return resolvedRouteData['data'].source;
-      })
-    )
-    .subscribe((pageData) => {
-      this.notifications = pageData;
-    }, (error) => console.log(error));
+  donations: any[];
+  currentUser: any;
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: DonateService,
+    private angularFire: AngularFireAuth
+  ) {
+    this.donations = [];
   }
 
+  ngOnInit(): void {
+    this.angularFire.onAuthStateChanged((user) => {
+      if (user) {
+        this.currentUser = user;
+        this.loadDonations();
+      } else {
+        this.currentUser = null;
+      }
+    });
+  }
+
+  loadDonations() {
+    this.firestore.getCollection("donations").subscribe((data) => {
+      if (this.currentUser) {
+        this.donations = data.filter(
+          (donation) => donation.userId === this.currentUser.email
+        );
+
+        console.log(this.donations);
+      }
+    });
+  }
 
   // NOTE: Ionic only calls ngOnDestroy if the page was popped (ex: when navigating back)
   // Since ngOnDestroy might not fire when you navigate from the current page, use ionViewWillLeave to cleanup Subscriptions
-  ionViewWillLeave(): void {
-    this.subscriptions.unsubscribe();
-  }
+  ionViewWillLeave(): void {}
 }
